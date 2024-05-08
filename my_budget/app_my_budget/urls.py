4ef981311dbfa201e4a6870a3a7ce735c272a76a
LeonyCoelho <leony.coelho@outlook.com>
2024-05-08 16:55:56 -0300

    bootstrap

--- my_budget/app_my_budget/urls.py
+++ my_budget/app_my_budget/urls.py
@@ -4,7 +4,9 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('new_transaction/', views.new_transaction, name='new_transaction'),
-    path('new_transfer/', views.new_transfer, name='new_transfer')
+    path('new_transfer/', views.new_transfer, name='new_transfer'),
+    path('delete_selected_accounts/', views.delete_selected_accounts, name='delete_selected_accounts'),
+    path('new_account/', views.new_account, name='new_account'),
 
 
 ]
--- my_budget/app_my_budget/views.py
+++ my_budget/app_my_budget/views.py
@@ -3,35 +3,85 @@ from app_my_budget.models import Account, Transaction
 from django.http import HttpResponseRedirect
 from django.views.decorators.http import require_GET
 from django.urls import reverse
-from django.db.models import F
+from django.db.models import F, Sum, Q
 from django.shortcuts import get_object_or_404
 from decimal import Decimal
 from django.contrib import messages
-
-
-
+from datetime import datetime
+from django.utils import timezone
+from django.http import HttpResponseBadRequest
 
 
 # def home(request):
 #     accounts = Account.objects.all()
+#     transactions = Transaction.objects.all()
 #     total_amount = sum(account.amount for account in accounts)
+
+#     current_month = datetime.now().month
+#     expenses_this_month = Transaction.objects.filter(
+#         transaction_type='Expense',
+#         paid=True,
+#         paid_at__month=current_month
+#     )
+#     total_expenses = expenses_this_month.aggregate(Sum('amount'))['amount__sum'] or 0
+
 #     context = {
-#         'accounts':accounts,
+#         'accounts': accounts,
+#         'transactions': transactions,
 #         'total_amount': total_amount,
+#         'total_expenses': total_expenses,
+#     }
+#     return render(request, 'home.html', context)
+
+
+# def home(request):
+#     accounts = Account.objects.annotate(
+#         total_amount=Sum('transaction__amount', filter=Q(transaction__paid=True))
+#     )
+
+#     transactions = Transaction.objects.all()
+
+#     # Calcular o saldo total
+#     total_amount = sum(account.total_amount for account in accounts if account.total_amount is not None)
+
+#     # Calcular as despesas totais no mês atual
+#     current_month = datetime.now().month
+#     total_expenses = Transaction.objects.filter(
+#         transaction_type='Expense',
+#         paid=True,
+#         paid_at__month=current_month
+#     ).aggregate(Sum('amount'))['amount__sum'] or 0
+
+#     context = {
+#         'accounts': accounts,
+#         'transactions': transactions,
+#         'total_amount': total_amount,
+#         'total_expenses': total_expenses,
 #     }
 #     return render(request, 'home.html', context)
 
 def home(request):
     accounts = Account.objects.all()
     transactions = Transaction.objects.all()
-    total_amount = sum(account.amount for account in accounts)
+
+    # Calcula o total de receitas (income)
+    total_income = sum(transaction.amount for transaction in transactions if transaction.transaction_type == 'Income' if transaction.paid)
+
+    # Calcula o total de despesas (expenses)
+    total_expenses = sum(transaction.amount for transaction in transactions if transaction.transaction_type == 'Expense' if transaction.paid)
+
+    # Calcula o saldo total subtraindo as despesas do total de receitas
+    total_balance = total_income - total_expenses
 
     context = {
         'accounts': accounts,
         'transactions': transactions,
-        'total_amount': total_amount,
+        'total_income': total_income,
+        'total_expenses': total_expenses,
+        'total_balance': total_balance,
     }
     return render(request, 'home.html', context)
+
 
 def new_transaction(request):
     if request.method == 'POST':
@@ -46,13 +96,29 @@ def new_transaction(request):
         paid = request.POST.get('paid', False) == 'on'
         paid_at = request.POST.get('paid_at')
         due_at = request.POST.get('due_at')
-        
+
+        # Converte o valor da quantia para um decimal
+        amount = Decimal(amount)
+
+        # Se 'paid_at' não estiver preenchido, use a data e hora atuais
+        if not paid_at:
+            paid_at = timezone.now()
+
+        if not due_at:
+            due_at = timezone.now()
+
+        # Se 'due_at' não for obrigatório caso 'paid' seja True
+        if not paid:
+            # Se 'due_at' não estiver preenchido e 'paid' for False, retorne um erro
+            if not due_at:
+                return HttpResponseBadRequest("Due date is required for unpaid transactions.")
+
         # Adicionar ou subtrair o valor da transação ao saldo da conta correspondente
         if transaction_type == 'Income':
             Account.objects.filter(id=account_id).update(amount=F('amount') + amount)
         elif transaction_type == 'Expense':
             Account.objects.filter(id=account_id).update(amount=F('amount') - amount)
-        
+
         # Crie e salve a nova transação no banco de dados
         new_transaction = Transaction.objects.create(
             name=name,
@@ -122,3 +188,30 @@ def new_transfer(request):
     else:
         # Se a requisição for GET, retorne um formulário vazio
         return redirect('home')
+    
+def new_account(request):
+    if request.method == 'POST':
+        # Coletando os dados do formulário
+        name = request.POST.get('name')
+        amount = request.POST.get('amount')
+        account_type = request.POST.get('account_type')
+
+        # Criando uma nova instância de Account com os dados do formulário
+        new_account = Account.objects.create(
+            name=name,
+            amount=amount,
+            account_type=account_type
+        )
+
+        # Redirecionando para a página inicial após a criação da conta
+        return redirect('home')
+
+    # Se o método HTTP não for POST, renderize a página do modal novamente
+    return render('home')
+    
+def delete_selected_accounts(request):
+    if request.method == 'POST':
+        selected_account_ids = request.POST.getlist('selected_accounts')
+        # Exclua as contas selecionadas do banco de dados
+        Account.objects.filter(id__in=selected_account_ids).delete()
+    return redirect('home')  # Redireciona de volta para a página inicial após a exclusão
